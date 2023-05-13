a466cd998e12f2a776891613d14ae2d3bbf2f864
Emilio Miralles <emilio_miralles@sfu.ca>
2023-05-13 05:42:23 +0200

    Update project config descriptors.

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,8 +2,8 @@ import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `Minimal Gatsby Tailwind Typescript`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `je-miralles.github.io`,
+    siteUrl: `https://je-miralles.github.io`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
